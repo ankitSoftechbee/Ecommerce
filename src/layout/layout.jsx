93990d4fec1278a0178da357831860e16d9c4f9a
Areeb Siraj <areebsiraj007@gmail.com>
2024-12-11 22:30:15 +0530

    Fixing Nav Bar

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,20 +1,16 @@
 import Header from './Header';
+import NavHeader from './NavHeader';
 import Sidebar from './Sidebar';
-import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
 
 function Layout() {
   return (
-    <div className="flex h-screen">
+    <div id="main-wrapper">
+      <NavHeader />
       <Header />
       <Sidebar />
-      <div class="content-body">
-
-        <div class="container-fluid">
-          <Outlet /> {/* Content will be injected here based on the current route */}
-        </div>
-        {/* <Footer /> */}
-      </div>
+      <Outlet /> {/* Content will be injected here based on the current route */}
+      {/* <Footer /> */}
     </div>
   );
 }
